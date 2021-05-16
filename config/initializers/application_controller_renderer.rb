323679a05b873e6a749695405c817651f71c8510
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-16 14:40:28 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b85f532443db0733fd1dcdfc42a0e32610cf1e08b39fbeb2bffce412c3a9a5dee1a97b40c06356057cdf88a84fa4954a4bba15432353f9276cdcd4df9f64ff8d'
